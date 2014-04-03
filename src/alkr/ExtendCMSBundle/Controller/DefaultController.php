30a345691b0480b4acf9c442d4b6d8e4a4efea46
alkr <sandello.alkr@gmail.com>
2014-04-03 16:58:16 +0300

    Добавил карту перенаправлений в роуты, исправил страницы об ошибках, вынес в src sass-ки, футер, хэдер и навбар
    Signed-off-by: alkr <sandello.alkr@gmail.com>

---
+++ src/alkr/ExtendCMSBundle/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace alkr\ExtendCMSBundle\Controller;
+
+use alkr\CMSBundle\Controller\DefaultController as BaseController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+class DefaultController extends BaseController
+{
+    /**
+     * @Route("/",name="index")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $response = parent::indexAction();
+        $response['template'] = 'full_width.html.twig';
+        return $response;
+    }
+}
