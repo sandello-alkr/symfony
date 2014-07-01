392cf80ffcb5379a27c660da16aea3021d4ec190
alkr <sandello.alkr@gmail.com>
2014-07-01 11:15:27 +0300

    мелкие исправления (путь jquery, защита dev)
    Signed-off-by: alkr <sandello.alkr@gmail.com>

--- web/dev.php
+++ web/dev.php
@@ -12,7 +12,7 @@ use Symfony\Component\Debug\Debug;
 // !!!разкомментить на продакшн сервере!!!
 /*if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('194.158.210.159', 'fe80::1', '::1'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
